b8ed4a758220bc5a43d06beb09269c672af159d4
Wojciech Sznapka <wojciech@sznapka.pl>
2011-09-26 00:36:08 +0200

    Added ugly none contact mailing handling

--- Newsletter/NewsletterManager.php
+++ Newsletter/NewsletterManager.php
@@ -6,27 +6,30 @@ use Doctrine\ORM\EntityManager;
 
 class NewsletterManager implements NewsletterManagerInterface
 {
-  protected $class;
-  protected $pheanstalk;
-  protected $preparationTube = "newsletter_preparation_tube";
-  protected $sendingTube     = "newsletter_sending_tube";
+    protected $class;
+    protected $pheanstalk;
+    protected $preparationTube = "newsletter_preparation_tube";
+    protected $sendingTube     = "newsletter_sending_tube";
 
-  public function __construct(EntityManager $em, $class)
-  {
-    $metadata = $em->getClassMetadata($class);
-    $this->class = $metadata->name;
+    public function __construct(EntityManager $em, $class)
+    {
+        $metadata = $em->getClassMetadata($class);
+        $this->class = $metadata->name;
 
-    $this->pheanstalk = new \Pheanstalk('127.0.0.1:11300');
-  }
-
-  public function putMailingInPreparationQueue($mailingId, array $contactIds)
-  {
-    foreach($contactIds as $contactId) {
-      $job = new \StdClass();
-      $job->contactId = $contactId;
-      $job->mailingId = $mailingId;
-      $job->contactClass = $this->class;
-      $this->pheanstalk->useTube($this->preparationTube)->put(json_encode($job));
+        $this->pheanstalk = new \Pheanstalk('127.0.0.1:11300');
     }
-  }
+
+    public function putMailingInPreparationQueue($mailingId, array $contactIds)
+    {
+        if (count($contactIds) == 0) {
+            throw new \InvalidArgumentException('No contacs selected, it need to be at least one contact to send mailing');
+        }
+        foreach($contactIds as $contactId) {
+              $job = new \StdClass();
+              $job->contactId = $contactId;
+              $job->mailingId = $mailingId;
+              $job->contactClass = $this->class;
+              $this->pheanstalk->useTube($this->preparationTube)->put(json_encode($job));
+        }
+    }
 }
